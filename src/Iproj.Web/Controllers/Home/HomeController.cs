4b1a414af6dbdf175cd9ff0b0941d5d29a500755
Muhammadqodir Yuldashev <muhammadqodir5050@gmail.com>
2024-09-08 11:53:24 +0500

    HOme page open Production

--- src/Iproj.Web/Controllers/Home/HomeController.cs
+++ src/Iproj.Web/Controllers/Home/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        if (_environment.IsDevelopment())
+        if (_environment.IsDevelopment() || _environment.IsProduction())
         {
             // only show in development
             return View();
